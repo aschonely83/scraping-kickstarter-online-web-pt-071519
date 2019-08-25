fc030a19879834f56980b567ed497680d2c7c342
Adam Schonely <aschonely@gmail.com>
2019-08-25 19:53:28 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,7 +3,6 @@ require "nokogiri"
 require "pry"
 
 def create_project_hash
-  create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   binding.pry
